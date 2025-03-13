5fb04a063de37315b5950a8cb856356ff192f6c5
JoanPRus <juanfranciscopalmarus@gmail.com>
2025-03-13 20:48:54 +0100

    practica embalse añadida con la plantilla

---
+++ Practica parcial/src/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>  
+#include <windows.h>
+#include <string.h>
+#include <stdbool.h>
+#include "rlutil.h"
+#include "llibreriaPropia.h"
+#include "llibreriaExercici.h"
+
+int main(){
+	SetConsoleOutputCP(CP_UTF8);
+	SetConsoleCP(CP_UTF8);
+	srand(time(NULL));
+	
+
+
+	acabament();
+	return 0;
+}
